b4726c37f92bb7a021cc282b4b5f78ecdd8f491d
Subin-Panta <subinpanta9@gmail.com>
2020-04-24 15:28:06 +0545

    added context to pass data through all components and added redcuers

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import './App.css';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 
 
 function App() {
   return (
     <div>
-     <Header />
-     <div className="container">
-       <Balance />
-       <IncomeExpenses />
-       <TransactionList />
-       <AddTransaction />
-     </div>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
     </div>
   );
 }
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,19 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import {GlobalContext} from "../context/GlobalState"
 export const IncomeExpenses = () => {
+    const {transactions} = useContext(GlobalContext)
+    const amounts = transactions.map((transaction) => transaction.amount)
+    const income = amounts.filter(item => item>0).reduce((acc,item) => (acc+=item),0).toFixed(2)
+    const expense = amounts.filter(item => item<0).reduce((acc,item) => (acc+=item),0).toFixed(2)
     return (
          <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p  className="money Plus">+Rs 0.00</p>
+                <p  className="money Plus">+Rs {income}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p  className="money minus">-Rs 0.00</p>
+                <p  className="money minus">-Rs {Math.abs(expense)}</p>
             </div>
         </div>
     )
